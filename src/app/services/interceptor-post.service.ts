1d35afaa71533c53e9bdd7acab984ed3fd7825a8
iisaiaso <isaiasibarraonofre@gmail.com>
2023-01-17 21:44:34 -0500

    Consumo del api post y estilos

--- src/app/services/interceptor-post.service.ts
+++ src/app/services/interceptor-post.service.ts
@@ -1,9 +1,19 @@
+import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
-export class InterceptorPostService {
+export class InterceptorPostService implements HttpInterceptor{
+
+  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> { 
+    console.log("Interceptado");
+    const newReq = req.clone({setHeaders:{
+      Authorization: 'Interceptado 00039830'
+    }})
+    return next.handle(newReq)
+  }
 
   constructor() { }
 }
