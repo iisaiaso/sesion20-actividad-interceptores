bbf785212369d3f0cce79059257d63d165b1429e
iisaiaso <isaiasibarraonofre@gmail.com>
2023-01-17 21:48:28 -0500

    init del proyecto final concluido con estilos

--- src/app/services/interceptor-post.service.ts
+++ src/app/services/interceptor-post.service.ts
@@ -5,13 +5,15 @@ import { Observable } from 'rxjs';
 @Injectable({
   providedIn: 'root'
 })
-export class InterceptorPostService implements HttpInterceptor{
+export class InterceptorPostService implements HttpInterceptor {
 
-  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> { 
+  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     console.log("Interceptado");
-    const newReq = req.clone({setHeaders:{
-      Authorization: 'Interceptado 00039830'
-    }})
+    const newReq = req.clone({
+      setHeaders: {
+        Authorization: 'Interceptado x000039830'
+      }
+    })
     return next.handle(newReq)
   }
 
